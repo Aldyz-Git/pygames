bb3148ad38fe881d5f2f5a570fc79ed6cb96c1a5
Aldy <aldertonz@live.co.uk>
2022-06-23 16:58:38 +0100

    simple car game images assets
    simple car game images assets lawn background, road & 2 cars and some adjustments to size & position. commit due to functioning state with basic assets

--- my-first-pygame/cargame.py
+++ my-first-pygame/cargame.py
@@ -3,11 +3,43 @@ from pygame.locals import *
 
 pygame.init()
 running = True
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((1920, 1000))
+pygame.display.set_caption("Aldyz car game")
+
+lawn = pygame.image.load("./images/lawn.jpg")
+lawn_x = 0
+lawn_y = 0
+lawn = pygame.transform.scale(lawn, (1920, 1000))
+screen.blit(lawn, (lawn_x, lawn_y))
+
+road = pygame.image.load("./images/road.jpg")
+road_x = 350
+road_y = 0
+road = pygame.transform.scale(road, (1920, 1200))
+road = pygame.transform.rotate(road, 90)
+screen.blit(road, (road_x, road_y))
+
+car = pygame.image.load("./images/car.png")
+car_loc = car.get_rect()
+car_x = 0
+car_y = 0
+car = pygame.transform.scale(car, (100, 200))
+screen.blit(car, (car_x, car_y))
+
+car2 = pygame.image.load("./images/car2.png")
+car2_loc = car2.get_rect()
+car2_x = 200
+car2_y = 200
+car2 = pygame.transform.scale(car2, (100, 200))
+screen.blit(car2, (car2_x, car2_y))
+
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+
+    screen.blit(car, car_loc)
+    pygame.display.update()
             
 pygame.quit()
