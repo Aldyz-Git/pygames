af6c2fe6445cc2d12fe9eefa4ce7563277da2bd0
Aldy <aldertonz@live.co.uk>
2022-06-23 18:55:05 +0100

    Update cargame.py

--- my-first-pygame/cargame.py
+++ my-first-pygame/cargame.py
@@ -19,27 +19,40 @@ road = pygame.transform.scale(road, (1920, 1200))
 road = pygame.transform.rotate(road, 90)
 screen.blit(road, (road_x, road_y))
 
-car = pygame.image.load("./images/car.png")
-car_loc = car.get_rect()
-car_x = 0
-car_y = 0
-car = pygame.transform.scale(car, (100, 200))
-screen.blit(car, (car_x, car_y))
+player = pygame.image.load("./images/car.png")
+player_loc = player.get_rect()
+playerX = 600
+playerY = 700
+vel = 10
+player = pygame.transform.scale(player, (100, 200))
+screen.blit(player, (playerX, playerY))
 
 car2 = pygame.image.load("./images/car2.png")
 car2_loc = car2.get_rect()
-car2_x = 200
-car2_y = 200
+car2_x = 1200
+car2_y = 700
+vel = 0.1
 car2 = pygame.transform.scale(car2, (100, 200))
 screen.blit(car2, (car2_x, car2_y))
-
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
-    screen.blit(car, car_loc)
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left!")
+                playerX = vel
+            if event.key == pygame.K_RIGHT:
+                print("Right!")
+                playerX = vel
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                vel = 0.1
+
+
+
+
     pygame.display.update()
             
 pygame.quit()
