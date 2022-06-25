0636dc5111215484c00e42f6f553be47cad8014f
Aldy <aldertonz@live.co.uk>
2022-06-25 18:49:58 +0100

    Update cargame.py

--- my-first-pygame/cargame.py
+++ my-first-pygame/cargame.py
@@ -9,20 +9,20 @@ screen = pygame.display.set_mode((1920, 1000))
 pygame.display.set_caption("Aldyz car game")
 
 
-lawn = pygame.image.load(os.path.join("my-first-pygame", "images", "lawn.jpg")).convert()
+lawn = pygame.image.load(os.path.join("images", "lawn.jpg")).convert()
 lawn_x = 0
 lawn_y = 0
 lawn = pygame.transform.scale(lawn, (1920, 1000))
 screen.blit(lawn, (lawn_x, lawn_y))
 
-road = pygame.image.load(os.path.join("my-first-pygame", "images", "road.jpg")).convert()
+road = pygame.image.load(os.path.join("images", "road.jpg")).convert()
 road_x = 350
 road_y = 0
 road = pygame.transform.scale(road, (1920, 1200))
 road = pygame.transform.rotate(road, 90)
 screen.blit(road, (road_x, road_y))
 
-car = pygame.image.load(os.path.join("my-first-pygame", "images", "car.png"))
+car = pygame.image.load(os.path.join("images", "car.png"))
 carX = 600
 carY = 700
 x_movement = 10
@@ -30,7 +30,7 @@ y_movement = 10
 car = pygame.transform.scale(car, (100, 200))
 screen.blit(car, (carX, carY))
 
-car2 = pygame.image.load(os.path.join("my-first-pygame", "images", "car2.png"))
+car2 = pygame.image.load(os.path.join("images", "car2.png"))
 car2_loc = car2.get_rect()
 car2_x = 1200
 car2_y = 700
